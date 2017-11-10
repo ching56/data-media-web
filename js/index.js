2bd580479147b8410d6fbfb676e5165a6222aed2
Ching <dino8556@gmail.com>
2017-11-10 11:34:47 +0800

    add help effect

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+$.fn.extend({
+  animateCss: function (animationName, callback) {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    this.addClass('animated ' + animationName).one(animationEnd, function () {
+      $(this).removeClass('animated ' + animationName);
+      if (callback) {
+        callback();
+      }
+    });
+    return this;
+  }
+});
+
 const icon = '<i class="fa fa-info-circle" aria-hidden="true"></i>'
 const info = `<h5>${icon} 本週因蘋果日報網站更新，部分資料有所缺漏</h5>`
 
@@ -22,6 +35,8 @@ var weekNum = tz.format('W')
 
 var s3Url = 'https://s3-ap-northeast-1.amazonaws.com/tw-media-data/report/'
 var objectUrl = s3Url + 'week_' + weekNum + '.json'
+
+let section = -1
 
 $('.page-container').hide()
 window.refreshCards();
@@ -106,12 +121,6 @@ $(document).ready(function () {
     setTimeout(function () {
       window.createNewsBarChart('#num-news-bar', barData);
     }, DELAY);
-
-    // var categoryData = {}
-    // media.forEach(function(d) {
-    //   categoryData[d] = report[d]
-    // })
-    // window.createCategory(categoryData);
 
     var myWordCloud = wordCloud('div.cloud');
     window.showNewWords(myWordCloud);
@@ -206,30 +215,28 @@ $('.menu').on('click', function() {
 $.get('dist/provocative_words.txt', function (data) {
   provocative_list = data.split('\n')
 })
+const SHOWDIST = $(window).height() * 0.75;
+const pos = $(window).scrollTop() + SHOWDIST;
+const updateAnimation = 'rotateIn'
 
 $('body').on('scroll', function(event) {
-  var SHOWDIST = $(window).height() * 0.75;
-  var pos = $(window).scrollTop() + SHOWDIST;
-  if (isPosBeyondIdTop(pos, '.cloud')) {
-    $('.legend-container').removeClass('hidden');
-  } else {
-    $('.legend-container').addClass('hidden');
-  }
 
-  if (isPosBeyondIdTop(pos, '#word-collection')) {
-    var title = '<h4>詞彙新聞集</h4>'
-    var content = title + '點擊用詞雲中的詞語，可以在詞彙新聞集中查看 6 家媒體報導此詞語的時間軸，與各自的新聞，其中<em>上色者為情緒性報導</em>。'
-    $('#help .content').html(content)
-  } else if (isPosBeyondIdTop(pos, '.cloud')){
+  if (isPosBeyondIdTop(pos, '.cloud') && section != 3){
     var title = '<h4>媒體用詞雲</h4>'
     var content = title + `統計 6 家媒體本週新聞標題內的用詞，在標題內<em>出現次數越多者字體越大</em>。`
+    content = content + '<h4>詞彙新聞集</h4>'
+    content = content + '點擊用詞雲中的詞語，可以在詞彙新聞集中查看 6 家媒體報導此詞語的時間軸，與各自的新聞，其中<em>上色者為情緒性報導</em>。'
+    section = 3
     $('#help .content').html(content)
-  } else if (isPosBeyondIdTop(pos, '.horizontal-lists')) {
+    $('#help').animateCss(updateAnimation)
+  } else if (isPosBeyondIdTop(pos, '.horizontal-lists') && !isPosBeyondIdTop(pos, '.cloud')  && section != 2) {
     var title = '<h4>標題用詞報告</h4>'
     var content = title + `<hr><strong>情緒性報導排行</strong>統計 6 家媒體本週情緒性報導比率的前五名，例如：「台灣  50%」，代表這家媒體所有標題含有「台灣」的報導，標題有 50% 含有情緒性用詞。</p>
 <hr><strong>少量報導主題</strong>分析同一詞語在 6 家媒體中，如果這家媒體對於這個詞語的關注度較低，即為少量報導主題。</p>`
+    section = 2
     $('#help .content').html(content)
-  } else if (isPosBeyondIdTop(pos, '.buzzwords')) {
+    $('#help').animateCss(updateAnimation)
+  } else if (isPosBeyondIdTop(pos, '.buzzwords') && !isPosBeyondIdTop(pos, '.horizontal-lists') && section != 1) {
     var title = '<h4>本週熱詞</h4>'
     var list_html = provocative_list.map(function(w){
       return '<li>' + w + '</li>'
@@ -240,8 +247,10 @@ $('body').on('scroll', function(event) {
 透過分析新聞資料的內文與標題，並以詞語向量化 ( word2vec ) 、群聚 ( cluster ) 的技術，找出與「酸」相近的用詞集，並作為以下定義的「情緒性用詞」。
 而情緒性報導是為，新聞標題中含有情緒性用詞的報導，此處統計各家媒體報導本週熱詞時情緒性報導的比率。「情緒性用詞」包含<ol>${list_html}
 </ol><hr><strong>報導數量</strong>統計 6 家媒體對本周熱詞的報導數量，分析其數量。`
+    section = 1
     $('#help .content').html(content)
-  } else {
+    $('#help').animateCss(updateAnimation)
+  } else if (!isPosBeyondIdTop(pos, '.buzzwords') && section != 0){
     var title = ''
     var content = title + `統計6家媒體於官方網站上，一周內所發表之新聞量。
     <hr><strong>資料來源</strong>
@@ -254,7 +263,10 @@ $('body').on('scroll', function(event) {
     <li>中國時報</li>
     </ol>
     `
+    section = 0
+    
     $('#help .content').html(content)
+    $('#help').animateCss(updateAnimation)
   }
 
 })
